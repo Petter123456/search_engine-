a277a950fc6551d11f92da6a9b5c7d8a1d8c6b7e
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-25 11:12:13 -0500

    added controller artist

--- spec/features/user_can_use_searches_spec.rb
+++ spec/features/user_can_use_searches_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "UserCanUseSearches", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "user can use search", type: :feature do
+  # pending "add some scenarios (or delete) #{__FILE__}" # Just a message
+  describe "goes to homepage" do
+    it "should have a search field" do
+      visit '/'
+      expect(page).to have_selector("input")
+    end
+  end
 end
