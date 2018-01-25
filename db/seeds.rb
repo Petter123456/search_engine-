94e894d9840b9a67de42429cd319bf17bbf98beb
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-25 11:15:05 -0500

    added artists to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Artist.destroy_all
+[
+  'The Doors',
+  'The Doors',
+  'Pink floyd',
+  'Ozzy osborne ',
+  'Tyga',
+  'Snoop dog',
+  'dr.dre'
+].each do |name|
+  Artist.create(name:name)
+end
